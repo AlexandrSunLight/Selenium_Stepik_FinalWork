f5a8c5aefaccb33f1d931aa9ef8dfbf2137e7928
AlexandrSunLight <AlexandrSunLight@gmail.com>
2020-04-16 00:02:10 +0300

    Move login page tests to main page -> login page transition test

--- test_main_page.py
+++ test_main_page.py
@@ -6,28 +6,14 @@ def test_guest_can_go_to_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com/"
     page = MainPage(browser, link)
     page.open()
+    time.sleep(5)
     page.go_to_login_page()
+    time.sleep(5)
+    login_page = LoginPage(browser, browser.current_url)
+    login_page.should_be_login_page()
 
 def test_guest_should_see_login_link(browser):
     link = "http://selenium1py.pythonanywhere.com/"
     page = MainPage(browser, link)
     page.open()
     page.should_be_login_link()
-    
-def test_guest_should_see_login_url(browser):
-    link = "http://selenium1py.pythonanywhere.com/accounts/login"
-    page = LoginPage(browser, link)
-    page.open()
-    page.should_be_login_url()
-    
-def test_guest_should_see_login_form(browser):
-    link = "http://selenium1py.pythonanywhere.com/accounts/login"
-    page = LoginPage(browser, link)
-    page.open()
-    page.should_be_login_form()
-    
-def test_guest_should_see_register_form(browser):
-    link = "http://selenium1py.pythonanywhere.com/accounts/login"
-    page = LoginPage(browser, link)
-    page.open()
-    page.should_be_register_form()
